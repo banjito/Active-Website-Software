007ebe2ba3d75a98afa06a2de1a0f46a6e5da7c8
jack-ampqes <jack.lyons@ampqes.com>
2026-07-05 21:51:52 -0500

    end of july 4th lol

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -159,14 +159,10 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
-        "spin-glow": {
-          to: { transform: "rotate(360deg)" },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "spin-glow": "spin-glow 3s linear infinite",
       },
     },
   },
